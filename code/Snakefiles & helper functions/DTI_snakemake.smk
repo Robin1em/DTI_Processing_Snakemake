5a7eea785b1b4011d1d2e4c5bbebc544c904e640
Robin <robin.e.mueller@gmx.de>
2024-09-02 13:36:31 +0200

    new folder structure

--- code/Snakefiles & helper functions/DTI_snakemake.smk
+++ code/Snakefiles & helper functions/DTI_snakemake.smk
@@ -5,12 +5,12 @@ import helper_functions
 # create config lists to determine the names of the input files for extracting ROIs in the first rule
 def generate_config():
     niftis = glob("origs/*AP*.nii*")
-    b_niftis = [x.replace("origs/", "").replace("_AP", "").replace("_iso", "").replace(".nii", "").replace(".gz", "") for x in niftis if not "_b0_" in x]
+    b_niftis = [x.replace("origs/", "").replace("_AP", "").replace("_iso", ""). replace(".nii", "").replace(".gz", "").replace("_2.2", "") for x in niftis if not "_b0_" in x]
     sample_ids = [x.split("_DTI_")[0] for x in b_niftis]
     b_values = [x.split("_DTI_")[1] for x in b_niftis]
     shortbvals = glob("bvecs_bvals/*short.bval")
     PA_niftis = glob("origs/*PA*nii*")
-    PA_names = [x.replace ("origs/", "").replace(".nii", "").replace(".gz", "").replace("_iso", "") for x in PA_niftis]
+    PA_names = [x.replace ("origs/", "").replace(".nii", "").replace(".gz", "").replace("_iso", "").replace("_2.2", "") for x in PA_niftis]
     jsons = glob("origs/*AP*json")
     bvecs = glob("origs/*.bvec")
     AP_b0s = [f for f in glob("*b0*.nii.gz") if not "PA" in f]
--- code/additional scripts/run_snakemake_in_multiple_dirs.sh
+++ code/additional scripts/run_snakemake_in_multiple_dirs.sh
@@ -9,9 +9,9 @@ run_snakemake() {
     echo "Running Snakemake in $dir"
     # Navigate to the directory
     cd "$dir" || return
-    # Run Snakemake with parallel execution in a detached mode
-    nohup snakemake --cores 2 -p --executor slurm --jobs 10 --default-resources mem_mb=1000 cpus_per_task=2 --snakefile /storage/biomeds/projects/neurorad-drg-dti/snakemake_code/DTI_snakemake.smk --configfile /storage/biomeds/projects/neurorad-drg-dti/snakemake_code/config.yml --latency-wait 1000 > "${dir}/snakemake.log" 2>&1 &
-    # Note: Redirect stdout and stderr to snakemake.log in the working directory
+    # Run Snakemake with parallel execution
+    # Adjust the number of jobs according to your system's capabilities
+    snakemake --cores 2  -p --executor slurm --jobs 10 --default-resources mem_mb=1000 cpus_per_task=2 --snakefile /storage/biomeds/projects/neurorad-drg-dti/snakemake_code/DTI_snakemake.smk --configfile /storage/biomeds/projects/neurorad-drg-dti/snakemake_code/config.yml --latency-wait 1000
     # Return to the parent directory after processing
     cd ..
 }
@@ -20,6 +20,11 @@ run_snakemake() {
 for dir in "$parent_dir"/*; do
     if [ -d "$dir" ]; then
         # Run Snakemake in the background
-        run_snakemake "$dir"
+        run_snakemake "$dir" &
     fi
 done
+
+# Wait for all background jobs to complete
+wait
+
+echo "All Snakemake workflows completed."
